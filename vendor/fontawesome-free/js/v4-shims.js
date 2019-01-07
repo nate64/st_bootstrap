31bb8bf46c30f07155555c35e7d04cd29d154056
Nate <nathanestes@gmail.com>
2019-01-07 14:07:03 -0700

    working through netlify deploy issues

--- vendor/fontawesome-free/js/v4-shims.js
+++ vendor/fontawesome-free/js/v4-shims.js
@@ -29,7 +29,7 @@
   var NAMESPACE_IDENTIFIER = '___FONT_AWESOME___';
   var PRODUCTION = function () {
     try {
-      return process.env.NODE_ENV === 'development';
+      return process.env.NODE_ENV === 'production';
     } catch (e) {
       return false;
     }
